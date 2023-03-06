1694e627526a666f254f6996ddb9933076403dca
Lorena Cueva <lorenacueva@gmail.com>
2023-03-06 14:30:13 -0800

    Adds avatar to Users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password, :password_confirmation)
+        params.permit(:username, :password, :password_confirmation, :avatar)
     end
 end
